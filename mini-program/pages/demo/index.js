e6b2e940b4745967b02062221264a6f8970afc6c
asdzsc <710972821@qq.com>
2020-06-27 14:16:07 +0800

    write-mini

---
+++ mini-program/pages/demo/index.js
@@ -0,0 +1,31 @@
+// pages/demo/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+	msg:"hello mina",
+	num:1000,
+	isGirl:false,
+	person:{
+		name:"mina",
+		age:18,
+		salary:5000
+	},
+	isChecked:false,
+	list:[
+		{
+			id:0,
+			name:"猪八戒"
+		},
+		{
+			id: 1,
+			name: "天蓬元帅"
+		}, {
+			id: 2,
+			name: "悟能"
+		},
+	]
+  }
+})
