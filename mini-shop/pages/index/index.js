fd8a9ee974a70b96e479efe8d0badc349110fdf9
asdzsc <710972821@qq.com>
2020-07-03 21:03:18 +0800

    mini-shop-category

--- mini-shop/pages/index/index.js
+++ mini-shop/pages/index/index.js
@@ -1,38 +1,61 @@
 //Page Object
+// 引入promise方法在小程序中路径要补全
+import {
+    request
+} from '../../request/index.js'
 Page({
     data: {
-
+        swiperList: [],
+        cateList: [],
+        floorList: []
     },
     //options(Object)
-    onLoad: function(options) {
+    onLoad: function (options) {
+        // wx.request({
+        //       url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+        //     method: 'GET',
+        //     success: (result) => {
+        //         // console.log(result);
+        //         // console.log(result.data.message);
+        //         this.setData({
+        //             swiperList: result.data.message
+        //         })
+        //     }
+        // });
+        this.getSwiperList();
+        this.getCateList();
+        this.getFloorList();
 
     },
-    onReady: function() {
+    async getSwiperList() {
+        request({
+            url: "/home/swiperdata",
+        }).then(result => {
+            this.setData({
+                swiperList: result
+            })
 
+        })
     },
-    onShow: function() {
+    getCateList() {
+        request({
+            url: "/home/catitems",
+        }).then(result => {
+            this.setData({
+                cateList: result
+            })
 
+        })
     },
-    onHide: function() {
+    getFloorList() {
+        request({
+            url: "/home/floordata",
+        }).then(result => {
+            this.setData({
+                floorList: result
+            })
 
+        })
     },
-    onUnload: function() {
 
-    },
-    onPullDownRefresh: function() {
-
-    },
-    onReachBottom: function() {
-
-    },
-    onShareAppMessage: function() {
-
-    },
-    onPageScroll: function() {
-
-    },
-    //item(index,pagePath,text)
-    onTabItemTap: function(item) {
-
-    }
 });
---
+++ mini-shop/request/index.js
@@ -0,0 +1,18 @@
+//  url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+
+export const request = (params) => {
+    const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
+    return new Promise((resolve, reject) => {
+        wx.request({
+            ...params,
+            url: baseUrl + params.url,
+            success: (result) => {
+                resolve(result.data.message);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+
+    })
+}
